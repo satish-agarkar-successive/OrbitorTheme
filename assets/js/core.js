9200ec8b8e8182aacef416662b94fcf4a02cde0b
mruduladdipalli <mruduladdipalli@gmail.com>
2020-04-25 20:26:03 +0530

    25 apr 2

--- assets/js/core.js
+++ assets/js/core.js
@@ -79,7 +79,7 @@ $(document).ready(function() {
 
         e.preventDefault();
 
-        //$(".infobar-settings-sidebar-overlay").css({"background": "rgba(0,0,0,0.4)", "position": "fixed"});
+        $(".infobar-settings-sidebar-overlay").css({"background": "rgba(0,0,0,0.4)", "position": "fixed"});
         
         //below lines are added in function getuserdetails() inside success
         // $("#infobar-edituser-sidebar").addClass("sidebarshow");
